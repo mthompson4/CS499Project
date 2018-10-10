4fbb79be233fc54ff8eba5c36a81ee7dbecac9b1
tdeeter25 <tommydeeter@uky.edu>
2018-10-10 01:24:05 -0400

    test commit

--- src/app/file-toggle/file-toggle.component.ts
+++ src/app/file-toggle/file-toggle.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Events } from 'ionic-angular';
 import { Observable } from 'rxjs';
+import { ActivatedRoute, Router, Event } from '@angular/router';
 
 @Component({
   selector: 'file-toggle',
@@ -11,11 +12,19 @@ import { Observable } from 'rxjs';
 
 export class FileToggleComponent{
 	items: Observable<any[]>;
-	constructor(db: AngularFireDatabase, public events: Events) {
+	constructor(
+		db: AngularFireDatabase, 
+		public events: Events,
+		private router: Router
+	) {
 		this.items = db.list('files').valueChanges();
 	}
 
-	clicked(filename){
+	fileClicked(){
+		console.log("clicked");
+		let filename = "index.html";
 		this.events.publish('file:toggled', filename);
+		let path = "/editor/" + filename;
+		this.router.navigate([path]);
 	}
 }
