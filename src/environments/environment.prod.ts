da469449b0ba268055379ad542b0029eedab27e4
tdeeter25 <tommydeeter@uky.edu>
2018-10-14 16:43:17 -0400

    added firebase hosting because GCP was misbehaving

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAKULc7VqbYUHAAehKR0bDf42WRLyTKch0",
+    authDomain: "test-project-5632e.firebaseapp.com",
+    databaseURL: "https://test-project-5632e.firebaseio.com",
+    projectId: "test-project-5632e",
+    storageBucket: "test-project-5632e.appspot.com",
+    messagingSenderId: "214812957898"
+  }
 };
